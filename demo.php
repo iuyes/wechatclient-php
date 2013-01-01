628c193c7978115e722bf321c9bf791d261ec38e
zhangv <derekzhangv@gmail.com>
2013-01-02 01:12:52 +0800

    comments

--- demo.php
+++ demo.php
@@ -3,15 +3,15 @@ define('TOKEN','YOUR_TOKEN');
 require_once 'WechatClient.php';
 class TestWechatListener extends WechatListener{
 
-	function onFirst($textRequest){
+	function onFirst($textRequest){ //用户第一次关注时
 	}
-	function onText(TextRequest $textRequest){
+	function onText(TextRequest $textRequest){ //用户发送文本内容时
 		return new TextResponse($textRequest->fromUserName,$textRequest->toUserName,$textRequest->content);
 	}
-	function onLocation(LocationRequest $locationRequest){
+	function onLocation(LocationRequest $locationRequest){ //用户发送位置信息时
 		return new TextResponse($locationRequest->fromUserName,$locationRequest->toUserName,$locationRequest->location_X.",".$locationRequest->location_Y);
 	}
-	function onImage(ImageRequest $imageRequest){
+	function onImage(ImageRequest $imageRequest){ //用户发送图片信息时
 		return new TextResponse($imageRequest->fromUserName,$imageRequest->toUserName,$imageRequest->picUrl);
 	}
 }
